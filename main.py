be01aab56841c01c8ae9da5068c864f0ec19b185
AlexeyKashintsev <akashintsev@yandex.ru>
2022-08-28 11:35:45 +0300

    Refactoring
    Code cleanup - added constants

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ from sklearn.ensemble import RandomForestRegressor
 from matplotlib import pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-train_data = pd.read_excel("C:/Users/rizhk/OneDrive/Desktop/Data/Train.xlsx")
+TRAIN_FILE_NAME = "C:/Users/rizhk/OneDrive/Desktop/Data/Train.xlsx"
+TEST_FILES_LOCATION = "C:/Users/rizhk/OneDrive/Desktop/test_input/"
+TEST_FILE_TEMPLATE = "Test_input_"
+TEST_FILES_COUNT = 4445
+
+train_data = pd.read_excel(TRAIN_FILE_NAME)
 columns = train_data.columns[1:]
 train_data.replace([np.inf, -np.inf], np.nan, inplace=True)
 train_data.fillna(0, inplace=True)
@@ -15,9 +20,8 @@ x_train = []
 x_test = []
 y = []
 
-file_count = 4445
-for file_number in range(file_count):
-    data = pd.read_excel(f"C:/Users/rizhk/OneDrive/Desktop/test_input/Test_input_{file_number+1}.xlsx")
+for file_number in range(TEST_FILES_COUNT):
+    data = pd.read_excel(f"{TEST_FILES_LOCATION}{TEST_FILE_TEMPLATE}{file_number+1}.xlsx")
     data = data.to_numpy()
     for i in range(data.shape[0]):
         for j in range(train_data.shape[0]):
